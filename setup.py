1c2ca1fe501abfae81b4f240ca6f40c230e923e3
Prasun <prasunboral@gmail.com>
2026-07-19 11:38:51 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prasun Baral",
+    author_email="prasunboral@gmail.com",
+    packages=find_packages()
+)
